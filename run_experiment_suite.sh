92f135370a82c83bf8aee47a1d1c502c501dadc5
Julian Legler <julian.legler@tu-berlin.de>
2024-11-19 15:41:50 +0100

    feat(experiment-suite): increase sleep timer after redeployment of SUE

--- run_experiment_suite.sh
+++ run_experiment_suite.sh
@@ -3,27 +3,27 @@ source venv/bin/activate
 helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
 sleep 30
 helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 30
+sleep 300
 python -m oxn experiments/recommendation_k8_base_1m_otel.yml --report reports/
 sleep 60
 
 helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
 sleep 30
 helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 30
+sleep 300
 python -m oxn experiments/recommendation_k8_delay_1m_otel.yml --report reports/
 sleep 60
 
 helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
 sleep 30
 helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 30
+sleep 300
 python -m oxn experiments/recommendation_k8_delay_1s_otel.yml --report reports/
 sleep 60
 
 helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
 sleep 30
 helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 30
+sleep 300
 python -m oxn experiments/recommendation_k8_sampling_5_percent.yml --report reports/
 sleep 60
