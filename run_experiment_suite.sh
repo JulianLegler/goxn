1bb5245eadbe1c395178bcd07af4e1d1fdee6fec
Julian Legler <julian.legler@tu-berlin.de>
2024-11-27 21:12:28 +0100

    refactor: cleaned the experiment run suite

--- run_experiment_suite.sh
+++ run_experiment_suite.sh
@@ -1,29 +1,59 @@
+#!/bin/sh
 source venv/bin/activate
 
-helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
-sleep 30
-helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 300
+cleanup() {
+    kubectl config set-context --current --namespace=system-under-evaluation
+    helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
+    sleep 30
+    kubectl config set-context --current --namespace=oxn-external-monitoring
+    helm uninstall kube-prometheus prometheus-community/kube-prometheus-stack
+    kubectl config set-context --current --namespace=system-under-evaluation
+    sleep 30
+}
+
+setup() {
+    kubectl config set-context --current --namespace=oxn-external-monitoring
+    helm install kube-prometheus prometheus-community/kube-prometheus-stack --namespace oxn-external-monitoring --create-namespace --version 62.5.1 -f values_kube_prometheus.yaml
+    sleep 30
+    kubectl config set-context --current --namespace=system-under-evaluation
+    helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
+    kubectl config set-context --current --namespace=system-under-evaluation
+    sleep 60
+}
+
+
+cleanup
+setup
 python -m oxn experiments/recommendation_k8_base_1m_otel.yml --report reports/
 sleep 60
 
-helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
-sleep 30
-helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 300
+cleanup
+setup
 python -m oxn experiments/recommendation_k8_delay_1m_otel.yml --report reports/
 sleep 60
 
-helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
-sleep 30
-helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 300
+cleanup
+setup
 python -m oxn experiments/recommendation_k8_delay_1s_otel.yml --report reports/
 sleep 60
 
-helm uninstall astronomy-shop open-telemetry/opentelemetry-demo
-sleep 30
-helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace -f values_opentelemetry_demo.yaml
-sleep 300
-python -m oxn experiments/recommendation_k8_sampling_5_percent.yml --report reports/
+cleanup
+setup
+python -m oxn experiments/recommendation_k8_base_5_percent.yml --report reports/
 sleep 60
+
+cleanup
+setup
+python -m oxn experiments/recommendation_k8_delay_5_percent.yml --report reports/
+sleep 60
+
+cleanup
+setup
+python -m oxn experiments/recommendation_k8_base_10_percent.yml --report reports/
+sleep 60
+
+cleanup
+setup
+python -m oxn experiments/recommendation_k8_delay_10_percent.yml --report reports/
+sleep 60
+
