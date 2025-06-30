141ce52f3a1b68dd74dc419ad6f3b352c02e610d
Julian Legler <julian.legler@tu-berlin.de>
2025-06-30 10:53:21 +0200

    feat: new experiments for persistence scenario feat: new metrics for network feat: new scenario for istio

--- run_experiment_suite.sh
+++ run_experiment_suite.sh
@@ -5,10 +5,22 @@ cleanup() {
     kubectl config set-context --current --namespace=system-under-evaluation
     helm uninstall astronomy-shop
     sleep 30
+    kubectl delete pvc data-astronomy-shop-elasticsearch-data-0
+    kubectl delete pvc data-astronomy-shop-elasticsearch-data-1
+    kubectl delete pvc data-astronomy-shop-elasticsearch-master-0
+    kubectl delete pvc data-astronomy-shop-elasticsearch-master-1
+    sleep 30
+    kubectl config set-context --current --namespace=isto-system
+    helm uninstall istio-base
+    helm uninstall istiod
+    helm uninstall istio-ingressgateway
+    sleep 30
     kubectl config set-context --current --namespace=oxn-external-monitoring
     helm uninstall kube-prometheus
     kubectl config set-context --current --namespace=system-under-evaluation
     sleep 30
+    
+
 }
 
 setup() {
@@ -24,6 +36,28 @@ setup() {
 setup_external_jaeger_backend() {
     kubectl config set-context --current --namespace=oxn-external-monitoring
     helm install kube-prometheus prometheus-community/kube-prometheus-stack --namespace oxn-external-monitoring --create-namespace --version 62.5.1 -f values_kube_prometheus.yaml
+    sleep 30
+    kubectl config set-context --current --namespace=system-under-evaluation
+    helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace --version 0.36.4 -f values_opentelemetry_demo_persistence.yaml
+    kubectl config set-context --current --namespace=system-under-evaluation
+    sleep 60
+}
+
+setup_external_jaeger_backend_istio() {
+    kubectl config set-context --current --namespace=oxn-external-monitoring
+    helm install kube-prometheus prometheus-community/kube-prometheus-stack --namespace oxn-external-monitoring --create-namespace --version 62.5.1 -f values_kube_prometheus.yaml
+    sleep 30
+    helm install istio-base istio/base --version 1.26.2 -n istio-system --set defaultRevision=default --create-namespace
+    helm install istiod istio/istiod -n istio-system \
+        --set global.proxy.autoInject=enabled \
+        --set meshConfig.enablePrometheusMerge=true \
+        --set values.pilot.resources.requests.cpu=100m \
+        --set meshConfig.enableTracing=true \
+        --set meshConfig.defaultConfig.tracing.sampling=100 \
+        --set meshConfig.defaultConfig.tracing.zipkin.address="jaeger-collector.system-under-evaluation.svc.cluster.local:9411" # "otel-collector.system-under-evaluation.svc.cluster.local:9411"
+    helm install istio-ingressgateway istio/gateway -n istio-system --create-namespace
+    # kubectl rollout restart deployment -n system-under-evaluation
+    kubectl label namespace system-under-evaluation istio-injection=enabled --overwrite
     sleep 30
     kubectl config set-context --current --namespace=system-under-evaluation
     helm install astronomy-shop open-telemetry/opentelemetry-demo --namespace system-under-evaluation --create-namespace --version 0.36.4 -f values_opentelemetry_demo_persistence.yaml
@@ -81,3 +115,10 @@ setup
 python -m oxn experiments/recommendation_k8_base_0_percent.yml --report reports/
 sleep 60
 
+cleanup
+setup_external_jaeger_backend_istio
+python -m oxn experiments/recommendation_k8_base_1m_otel_persistence_istio.yml --report reports/
+sleep 60
+
+#cleanup
+#setup_external_jaeger_backend_istio
